b3593fb648422c0daa7896b727267b40a0d59cf7
LiYuze <228487099@qq.com>
2023-08-30 17:47:32 +0800

    引入kafka-stream

---
+++ yz-leadnews-basic/spring-boot-starter-kafka/src/main/java/com/leadnews/kafka/config/KafkaConfig.java
@@ -0,0 +1,41 @@
+package com.leadnews.kafka.config;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.kafka.streams.StreamsConfig.*;
+
+/**
+ * @author lihaohui
+ * @date 2023/8/30
+ */
+@Configuration
+@EnableKafkaStreams
+public class KafkaConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Value(value = "${spring.kafka.stream.groupId}")
+    private String groupId;
+
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    KafkaStreamsConfiguration kStreamsConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(APPLICATION_ID_CONFIG, groupId + "_stream_aid");
+        props.put(CLIENT_ID_CONFIG, groupId + "_stream_cid");
+        props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        return new KafkaStreamsConfiguration(props);
+    }
+}
